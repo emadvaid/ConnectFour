884b0c1c643cded18bc84eb19be9a1f7655179c4
mtwashingtontutoring <mtwashingtontutoring@yahoo.com>
2018-12-02 18:55:50 -0500

    fixed the issue with reporting wrong results

--- src/test/java/org/emadvaid/connect_four/PlayerTest.java
+++ src/test/java/org/emadvaid/connect_four/PlayerTest.java
@@ -2,6 +2,8 @@ package org.emadvaid.connect_four;
 
 public class PlayerTest {
 
+    private static int fails;
+
     /*
      *  Test case that test creating a player object.
      */
@@ -23,14 +25,32 @@ public class PlayerTest {
         //   and save it to a variable named player1 of type Player
         Player player1 = new Player("Player1");
 
-        player1.addWin();
-
         assertNotNull("player1", player1 == null);
         assertEquals("player1.getName()","Player1", player1.getName());
-        assertEquals("player1.getWins()", 1, player1.getWins());
-        assertEquals("player1.getTotalGamesPlayed()", 1, player1.getTotalGamesPlayed());
-        assertEquals("player1.getWinRatio()", 1.0, player1.getWinRatio());
-        assertEquals("player1.getLossRatio()", 0.0, player1.getLossRatio());
+
+        player1.addWin();
+        assertEquals("after 1 win - player1.getWins()", 1, player1.getWins());
+        assertEquals("after 1 win - player1.getTotalGamesPlayed()", 1, player1.getTotalGamesPlayed());
+        assertEquals("after 1 win - player1.getWinRatio()", 1.0, player1.getWinRatio());
+        assertEquals("after 1 win - player1.getLossRatio()", 0.0, player1.getLossRatio());
+
+        player1.addWin();
+        assertEquals("after 2 wins - player1.getWins()", 2, player1.getWins());
+        assertEquals("after 2 wins - player1.getTotalGamesPlayed()", 2, player1.getTotalGamesPlayed());
+        assertEquals("after 2 wins - player1.getWinRatio()", 1.0, player1.getWinRatio());
+        assertEquals("after 2 wins - player1.getLossRatio()", 0.0, player1.getLossRatio());
+
+        player1.addWin();
+        assertEquals("after 3 wins - player1.getWins()", 3, player1.getWins());
+        assertEquals("after 3 wins - player1.getTotalGamesPlayed()", 3, player1.getTotalGamesPlayed());
+        assertEquals("after 3 wins - player1.getWinRatio()", 1.0, player1.getWinRatio());
+        assertEquals("after 3 wins - player1.getLossRatio()", 0.0, player1.getLossRatio());
+
+        player1.addWin();
+        assertEquals("after 4 wins - player1.getWins()", 4, player1.getWins());
+        assertEquals("after 4 wins - player1.getTotalGamesPlayed()", 4, player1.getTotalGamesPlayed());
+        assertEquals("after 4 wins - player1.getWinRatio()", 1.0, player1.getWinRatio());
+        assertEquals("after 4 wins - player1.getLossRatio()", 0.0, player1.getLossRatio());
     }
 
     public void testAddLossToPlayer() {
@@ -38,14 +58,32 @@ public class PlayerTest {
         //   and save it to a variable named player1 of type Player
         Player player1 = new Player("Player1");
 
-        player1.addLoss();
-
         assertNotNull("player1", player1 == null);
         assertEquals("player1.getName()","Player1", player1.getName());
-        assertEquals("player1.getWins()", 0, player1.getWins());
-        assertEquals("player1.getTotalGamesPlayed()", 1, player1.getTotalGamesPlayed());
-        assertEquals("player1.getWinRatio()", 0.0, player1.getWinRatio());
-        assertEquals("player1.getLossRatio()", 1.0, player1.getLossRatio());
+
+        player1.addLoss();
+        assertEquals("after 1 loss - player1.getWins()", 0, player1.getWins());
+        assertEquals("after 1 loss - player1.getTotalGamesPlayed()", 1, player1.getTotalGamesPlayed());
+        assertEquals("after 1 loss - player1.getWinRatio()", 0.0, player1.getWinRatio());
+        assertEquals("after 1 loss - player1.getLossRatio()", 1.0, player1.getLossRatio());
+
+        player1.addLoss();
+        assertEquals("after 2 losses - player1.getWins()", 0, player1.getWins());
+        assertEquals("after 2 losses - player1.getTotalGamesPlayed()", 2, player1.getTotalGamesPlayed());
+        assertEquals("after 2 losses - player1.getWinRatio()", 0.0, player1.getWinRatio());
+        assertEquals("after 2 losses - player1.getLossRatio()", 1.0, player1.getLossRatio());
+
+        player1.addLoss();
+        assertEquals("after 3 losses - player1.getWins()", 0, player1.getWins());
+        assertEquals("after 3 losses - player1.getTotalGamesPlayed()", 3, player1.getTotalGamesPlayed());
+        assertEquals("after 3 losses - player1.getWinRatio()", 0.0, player1.getWinRatio());
+        assertEquals("after 3 losses - player1.getLossRatio()", 1.0, player1.getLossRatio());
+
+        player1.addLoss();
+        assertEquals("after 4 losses - player1.getWins()", 0, player1.getWins());
+        assertEquals("after 4 losses - player1.getTotalGamesPlayed()", 4, player1.getTotalGamesPlayed());
+        assertEquals("after 4 losses - player1.getWinRatio()", 0.0, player1.getWinRatio());
+        assertEquals("after 4 losses - player1.getLossRatio()", 1.0, player1.getLossRatio());
     }
 
     public static void main(String args[]){
@@ -118,8 +156,16 @@ public class PlayerTest {
         }
     }
 
+    protected void assertEquals(String name, double expected, double actual) {
+        if(Math.abs(expected - actual) > .00001) {
+            throw new RuntimeException(name + ": Assert equals failed"
+                    + " expected = " + expected
+                    + " actual = " + actual);
+        }
+    }
+
     protected void assertEquals(String name, float expected, float actual) {
-        if(Math.abs(expected - actual) < .00001) {
+        if(Math.abs(expected - actual) > .00001) {
             throw new RuntimeException(name + ": Assert equals failed"
                     + " expected = " + expected
                     + " actual = " + actual);
